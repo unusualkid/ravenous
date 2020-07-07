2faa5d59d8d21b0d6a25b5b84e3d541e59643581
Kenneth Chen <unusualkid@gmail.com>
2020-07-07 16:37:03 +0800

    Add sortByOptions to searchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,3 +1,8 @@
 import React from "react";
 import "./SearchBar.css";
 
+let sortByOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "reivew_count"
+}
