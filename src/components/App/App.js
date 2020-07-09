d10a3c3a2fc657219021f567a57b61b784758755
Kenneth Chen <unusualkid@gmail.com>
2020-07-09 17:00:52 +0800

    Complete part2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,19 @@ import '../SearchBar/SearchBar'
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+let business = {
+	imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+	name: 'MarginOtto Pizzeria',
+	address: '1010 Paddington Way',
+	city: 'Flavortown',
+	state: 'NY',
+	zipCode: '10101',
+	category: 'Italian',
+	rating: 4.5,
+	reviewCount: 90
+};
+
+let businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
   render() {
@@ -13,7 +26,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business"
 
+function business(business) {
+    return <Business business={business}/>;
+}
+
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {this.props.businesses.map(business)}
             </div>
         );
     }
