d2d25af10f7c063dfab8c62876502ec9caa77ca6
Kenneth Chen <unusualkid@gmail.com>
2020-07-10 10:25:32 +0800

    Refactor

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business"
 
-function business(business) {
-    return <Business business={business}/>;
-}
-
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                {this.props.businesses.map(business)}
+                {this.props.businesses.map(business => {
+                    return <Business business={business}/>;
+                })}
             </div>
         );
     }
